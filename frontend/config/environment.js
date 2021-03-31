6912b5bd04c4f16c9860684918a7799e34892b03
Mikhail <misha.smi1996@yandex.ru>
2021-03-31 19:41:48 +0500

    FEAT: electron app (#14)

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -4,8 +4,8 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: "knowledge-shell",
     environment,
-    rootURL: "/",
-    locationType: "auto",
+    rootURL: process.env.EMBER_CLI_ELECTRON ? '' : "/",
+    locationType: process.env.EMBER_CLI_ELECTRON ? 'hash' : "auto",
     EmberENV: {
       FEATURES: {},
       EXTEND_PROTOTYPES: {
