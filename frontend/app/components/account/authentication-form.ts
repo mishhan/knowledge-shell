6ddc98d13e1bb6bf6e96eab2ce974717c4e46184
Misha <misha.smi1996@yandex.ru>
2021-08-14 16:12:07 +0500

    fix eslint errors

--- frontend/app/components/account/authentication-form.ts
+++ frontend/app/components/account/authentication-form.ts
@@ -4,60 +4,60 @@ import { action } from "@ember/object";
 import userAuthenticationValidator from "knowledge-shell/validations/user-authentication";
 
 interface AccountAuthenticationFormArgs {
-  authenticate: (identification: string, password: string) => void;
-  isLoading: boolean;
+	authenticate: (identification: string, password: string) => void;
+	isLoading: boolean;
 }
 
 export default class AccountAuthenticationForm extends Component<AccountAuthenticationFormArgs> {
-  @tracked isSubmitted!: boolean;
+	@tracked isSubmitted!: boolean;
 
-  @tracked identification!: string;
-  @tracked password!: string;
-  @tracked showPassword = false;
-  @tracked validator = userAuthenticationValidator.get();
+	@tracked identification!: string;
+	@tracked password!: string;
+	@tracked showPassword = false;
+	@tracked validator = userAuthenticationValidator.get();
 
-  get identificationValidationErrors(): string[] {
-    const identificationValidationErrors = this.validator.getErrors("identification");
-    return identificationValidationErrors;
-  }
+	get identificationValidationErrors(): string[] {
+		const identificationValidationErrors = this.validator.getErrors("identification");
+		return identificationValidationErrors;
+	}
 
-  get passwordValidationErrors(): string[] {
-    const passwordValidationErrors = this.validator.getErrors("password");
-    return passwordValidationErrors;
-  }
+	get passwordValidationErrors(): string[] {
+		const passwordValidationErrors = this.validator.getErrors("password");
+		return passwordValidationErrors;
+	}
 
-  @action
-  onSubmit(event: Event): void {
-    event.preventDefault();
-    this.isSubmitted = true;
-    this.validateForm();
-    const hasValidationErrors = this.validator.hasErrors();
-    if (!hasValidationErrors) {
-      const { identification, password } = this;
-      this.args.authenticate(identification, password);
-    }
-  }
+	@action
+	onSubmit(event: Event): void {
+		event.preventDefault();
+		this.isSubmitted = true;
+		this.validateForm();
+		const hasValidationErrors = this.validator.hasErrors();
+		if (!hasValidationErrors) {
+			const { identification, password } = this;
+			this.args.authenticate(identification, password);
+		}
+	}
 
-  @action
-  onIdentificationChange(value: string): void {
-    this.identification = value;
-    this.validateForm("identification");
-  }
+	@action
+	onIdentificationChange(value: string): void {
+		this.identification = value;
+		this.validateForm("identification");
+	}
 
-  @action
-  onPasswordChange(value: string): void {
-    this.password = value;
-    this.validateForm("password");
-  }
+	@action
+	onPasswordChange(value: string): void {
+		this.password = value;
+		this.validateForm("password");
+	}
 
-  validateForm(fieldName?: string): void {
-    const { identification, password } = this;
-    this.validator = userAuthenticationValidator(
-      {
-        identification,
-        password,
-      },
-      fieldName,
-    );
-  }
+	validateForm(fieldName?: string): void {
+		const { identification, password } = this;
+		this.validator = userAuthenticationValidator(
+			{
+				identification,
+				password,
+			},
+			fieldName,
+		);
+	}
 }
--- frontend/app/components/account/registration-form.ts
+++ frontend/app/components/account/registration-form.ts
@@ -4,80 +4,80 @@ import { action } from "@ember/object";
 import userRegistrationValidator from "knowledge-shell/validations/user-registration";
 
 interface AccountRegistrationFormArgs {
-  registr: (userName: string, email: string, password: string) => void;
-  isLoading: boolean;
+	registr: (userName: string, email: string, password: string) => void;
+	isLoading: boolean;
 }
 
 export default class AccountRegistrationForm extends Component<AccountRegistrationFormArgs> {
-  @tracked isSubmitted!: boolean;
+	@tracked isSubmitted!: boolean;
 
-  @tracked userName!: string;
-  @tracked email!: string;
-  @tracked password!: string;
-  @tracked showPassword = false;
-  @tracked acceptTerms!: boolean;
-  @tracked validator = userRegistrationValidator.get();
+	@tracked userName!: string;
+	@tracked email!: string;
+	@tracked password!: string;
+	@tracked showPassword = false;
+	@tracked acceptTerms!: boolean;
+	@tracked validator = userRegistrationValidator.get();
 
-  get userNameValidationErrors(): string[] {
-    const userNameValidationErrors = this.validator.getErrors("userName");
-    return userNameValidationErrors;
-  }
+	get userNameValidationErrors(): string[] {
+		const userNameValidationErrors = this.validator.getErrors("userName");
+		return userNameValidationErrors;
+	}
 
-  get emailValidationErrors(): string[] {
-    const emailValidationErrors = this.validator.getErrors("email");
-    return emailValidationErrors;
-  }
+	get emailValidationErrors(): string[] {
+		const emailValidationErrors = this.validator.getErrors("email");
+		return emailValidationErrors;
+	}
 
-  get passwordValidationErrors(): string[] {
-    const passwordValidationErrors = this.validator.getErrors("password");
-    return passwordValidationErrors;
-  }
+	get passwordValidationErrors(): string[] {
+		const passwordValidationErrors = this.validator.getErrors("password");
+		return passwordValidationErrors;
+	}
 
-  get acceptTermsValidationErrors(): string[] {
-    const acceptTermsValidationErrors = this.validator.getErrors("acceptTerms");
-    return acceptTermsValidationErrors;
-  }
+	get acceptTermsValidationErrors(): string[] {
+		const acceptTermsValidationErrors = this.validator.getErrors("acceptTerms");
+		return acceptTermsValidationErrors;
+	}
 
-  @action
-  onSubmit(event: Event): void {
-    event.preventDefault();
-    this.isSubmitted = true;
-    this.validateForm();
-    const hasValidationErrors = this.validator.hasErrors();
-    if (!hasValidationErrors) {
-      const { userName, email, password } = this;
-      this.args.registr(userName, email, password);
-    }
-  }
+	@action
+	onSubmit(event: Event): void {
+		event.preventDefault();
+		this.isSubmitted = true;
+		this.validateForm();
+		const hasValidationErrors = this.validator.hasErrors();
+		if (!hasValidationErrors) {
+			const { userName, email, password } = this;
+			this.args.registr(userName, email, password);
+		}
+	}
 
-  @action
-  onUsernameChange(value: string): void {
-    this.userName = value;
-    this.validateForm("userName");
-  }
+	@action
+	onUsernameChange(value: string): void {
+		this.userName = value;
+		this.validateForm("userName");
+	}
 
-  @action
-  onEmailChange(value: string): void {
-    this.email = value;
-    this.validateForm("email");
-  }
+	@action
+	onEmailChange(value: string): void {
+		this.email = value;
+		this.validateForm("email");
+	}
 
-  @action
-  onPasswordChange(value: string): void {
-    this.password = value;
-    this.validateForm("password");
-  }
+	@action
+	onPasswordChange(value: string): void {
+		this.password = value;
+		this.validateForm("password");
+	}
 
-  validateForm(fieldName?: string): void {
-    const { userName, email, password, acceptTerms } = this;
-    this.validator = userRegistrationValidator(
-      {
-        userName,
-        email,
-        password,
-        acceptTerms,
-      },
-      fieldName,
-    );
-  }
+	validateForm(fieldName?: string): void {
+		const { userName, email, password, acceptTerms } = this;
+		this.validator = userRegistrationValidator(
+			{
+				userName,
+				email,
+				password,
+				acceptTerms,
+			},
+			fieldName,
+		);
+	}
 }
