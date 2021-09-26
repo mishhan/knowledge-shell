b3793e84a466145d74cd9cdaba6a35b7ecba9be1
Misha <misha.smi1996@yandex.ru>
2021-09-26 15:38:13 +0500

    fix rule reordering

--- frontend/app/controllers/app/production-base/rules/index.ts
+++ frontend/app/controllers/app/production-base/rules/index.ts
@@ -1,10 +1,29 @@
 import Controller from "@ember/controller";
-import { action } from "@ember/object";
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
+import { action, computed } from "@ember/object";
 import { Rule } from "knowledge-shell/models";
 
 export default class AppProductionBaseRulesIndex extends Controller {
+	@computed("model.rules")
 	get rules(): Rule[] {
-		return this.model.rules;
+		const { rules } = this.model;
+		const orderedRules = rules.sortBy("order");
+		return orderedRules;
+	}
+
+	@computed("rules.@each.hasDirtyAttributes")
+	get enableSave(): boolean {
+		const enableSave = this.rules.any((rule: Rule) => rule.get("hasDirtyAttributes"));
+		return enableSave;
+	}
+
+	@action
+	async saveRules(): Promise<void> {
+		this.rules
+			.filter((rule: Rule) => rule.hasDirtyAttributes)
+			.forEach(async (rule: Rule) => {
+				await rule.save();
+			});
 	}
 
 	@action
