d99f9041d40eb87aeba1c3194900062fe0e70d4d
Mikhail <misha.smi1996@yandex.ru>
2022-05-22 18:31:25 +0500

    update db dump

--- backend/KnowledgeShell.Api/Data/Scripts/delete.sql
+++
@@ -1,6 +0,0 @@
-﻿delete from public.slots;
-delete from public.productions;
-delete from public.domains;
-delete from public.domain_values;
-delete from public.frames;
-delete from public.positions;
